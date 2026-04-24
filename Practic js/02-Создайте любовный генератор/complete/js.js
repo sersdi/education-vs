5aed18ee679762be94bf823a99393c3aa5f929b7
Sergey Ivanov <141302544+sersdi@users.noreply.github.com>
2026-04-24 19:03:28 +0700

    Add files via upload

---
+++ Practic js/02-Создайте любовный генератор/complete/js.js
@@ -0,0 +1,11 @@
+const firstName = prompt("Напишите первое имя"),
+    secondName = prompt("Напишите второе имя")
+
+const randomNumber = (Math.floor(Math.random() * 100))
+
+
+
+alert(`Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}`)
+
+
+
