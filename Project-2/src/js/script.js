08206216701ede1e829312b119781e13991b7fe8
Sergey Ivanov <141302544+sersdi@users.noreply.github.com>
2026-04-24 19:04:22 +0700

    Add files via upload

---
+++ Project-2/src/js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu'),
+    menuItem = document.querySelectorAll('.nav-menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+})
+/* Код для анимации прокрутки страницы к якорю */
+var $page = $('html, body')
+$('a[href*="#"]').click(function () {
+	$page.animate(
+		{
+			scrollTop: $($.attr(this, 'href')).offset().top,
+		},
+		750
+	)
+	return false
+})
